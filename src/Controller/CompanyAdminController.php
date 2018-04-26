1e65b89861ab2c70fc02f52580da878f19ce92ea
Damien SEDIRA <es.todo78@yahoo.fr>
2018-04-26 17:35:20 +0200

    partie admin Entreprise

--- src/Controller/CompanyAdminController.php
+++ src/Controller/CompanyAdminController.php
@@ -10,6 +10,7 @@
 namespace Controller;
 
 use Model\CompanyManager;
+use Structures\Notification;
 
 /**
  * Class ItemController
@@ -23,22 +24,30 @@ class CompanyAdminController extends AbstractController
      */
     public function index()
     {
+        $notification = (new notification())->getNotification('notification');
         $companyManager = new CompanyManager();
         $info = $companyManager->selectFirst();
-        return $this->twig->render('admin/company.html.twig', ['company' => $info]);
+        return $this->twig->render('admin/company.html.twig',
+            ['company' => $info, 'notification' => $notification]);
     }
 
     public function edit()
     {
+        $notification = new Notification();
         if (!empty($_POST)) {
             foreach ($_POST as $key => $value) {
                 $cleanPost[$key] = trim($value);
             }
             $companyManager = new CompanyManager();
-            if (preg_match('`^0[1-79]([-. ]?[0-9]{2}){4}$`', $cleanPost['phone']) &&
-                filter_var($cleanPost['facebook'], FILTER_VALIDATE_URL) &&
-                filter_var($cleanPost['tweeter'], FILTER_VALIDATE_URL) &&
-                filter_var($cleanPost['instagram'], FILTER_VALIDATE_URL)) {
+            if (!preg_match('`^0[1-79]([-. ]?[0-9]{2}){4}$`', $cleanPost['phone'])) {
+                $notification->setNotification('danger', 'Le numero de telephone n\'est pas valide');
+            } elseif (filter_var($cleanPost['facebook'], FILTER_VALIDATE_URL) == false) {
+                $notification->setNotification('danger', 'L\'URL de facebook n\'est pas valide');
+            } elseif (filter_var($cleanPost['tweeter'], FILTER_VALIDATE_URL) == false) {
+                $notification->setNotification('danger', 'L\URL de tweeter n\'est pas valide');
+            } elseif (filter_var($cleanPost['instagram'], FILTER_VALIDATE_URL) == false) {
+                $notification->setNotification('danger', 'L\'URL d\'instagram n\'est pas valide');
+            } else {
                 $companyManager->update($cleanPost['id'], [
                     'phone' => $cleanPost['phone'],
                     'facebook' => $cleanPost['facebook'],
@@ -46,9 +55,7 @@ class CompanyAdminController extends AbstractController
                     'instagram' => $cleanPost['instagram'],
                     'mentions_legales' => $cleanPost['mentions_legales'],
                 ]);
-
             }
-
         }
         header('location:/admin/company');
         exit();
